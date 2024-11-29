5e3a547f73078a5252e5e49bba2729eff69f9a5c
Sven Pachnit <sven@bmonkeys.net>
2024-11-30 00:25:59 +0100

    Test

--- ext/wikitext/parser.c
+++ ext/wikitext/parser.c
@@ -2586,15 +2586,22 @@ VALUE Wikitext_parser_parse(int argc, VALUE *argv, VALUE self)
         token = NULL;
     } while (1);
 return_output:
-    // nasty hack to avoid re-allocating our return value
-    str_append(parser->output, null_str, 1); // null-terminate
-    len = parser->output->len - 1; // don't count null termination
+    // // nasty hack to avoid re-allocating our return value
+    // str_append(parser->output, null_str, 1); // null-terminate
+    // len = parser->output->len - 1; // don't count null termination
 
-    VALUE out = rb_str_buf_new(RSTRING_EMBED_LEN_MAX + 1);
-    free(RSTRING_PTR(out));
-    RSTRING(out)->as.heap.aux.capa = len;
-    RSTRING(out)->as.heap.ptr = parser->output->ptr;
-    RSTRING(out)->as.heap.len = len;
-    parser->output->ptr = NULL; // don't double-free
+    // VALUE out = rb_str_buf_new(RSTRING_EMBED_LEN_MAX + 1);
+    // free(RSTRING_PTR(out));
+    // RSTRING(out)->as.heap.aux.capa = len;
+    // RSTRING(out)->as.heap.ptr = parser->output->ptr;
+    // RSTRING(out)->as.heap.len = len;
+    // parser->output->ptr = NULL; // don't double-free
+    // return out;
+    str_append(parser->output, null_str, 1);  // Null-terminate
+    len = parser->output->len - 1;           // Exclude null terminator
+
+    VALUE out = rb_str_new(parser->output->ptr, len);  // Create string from ptr
+    parser->output->ptr = NULL;                        // Prevent double-free
+
     return out;
 }
